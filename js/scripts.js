cfa5352ca2d2765145a27ab1932e3957dfbdfcff
Noah Yasskin <yasskin@icloud.com>
2016-06-17 10:49:09 -0700

    add working radio and checkbox inputs in HTML and JS / jQuery UI

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,25 @@
 // Business (or back-end) logic:
 var pizzaToppings = [];
+var pizzaSize;
+var pizzaCost;
 
-function Pizza() {
-  this.pizzaToppings = pizzaToppings;
-  this.pizzaSize = pizzaSize;
+function Pizza(size, toppings, sizeCost, toppingsCost) {
+  this.pizzaSize = size;
+  this.pizzaToppings = toppings;
   this.pizzaCost = pizzaCost;
 }
 
-Pizza.prototype.sizeCost() {
+Pizza.prototype.sizeCost = function() {
   if (this.pizzaSize === "Small") {
     return this.pizzaCost += 10;
   } else if (this.pizzaSize === "Medium") {
-    return this.pizzaCost += 15; :
+    return this.pizzaCost += 15;
   } else { // for "Large" pizza
     return this.pizzaCost += 20;
   }
 }
 
-Pizza.prototype.toppingsCost() {
+Pizza.prototype.toppingsCost = function() {
   if (this.pizzaSize === "Small") {
     for (i=0; i<this.pizzaToppings.length; i++) {
       this.pizzaCost += 1;
@@ -25,12 +27,12 @@ Pizza.prototype.toppingsCost() {
   }
   if (this.pizzaSize === "Medium") {
     for (i=0; i<thisPizzaToppings.length; i++) {
-      this.finalCost += 2;
+      this.pizzaCost += 2;
     }
   }
   if (this.pizzaSize === "Large") {
     for (i=0; i<pizzaToppings.length;) {
-      this.finalCost += 3;
+      this.pizzaCost += 3;
     }
   }
 }
@@ -38,20 +40,29 @@ Pizza.prototype.toppingsCost() {
 // Everything below this line is user interface (or front-end) logic:
 
 $(document).ready(function() {
-
-  $("size-form#pizza-size").submit(function(event) {
+  $("form").submit(function(event) {
     event.preventDefault();
-    var pizzaSize = $("input#pizza-size").val();
+    //collect the user choices
+    var size = $("input:radio[name=size]:checked").val();
+    console.log(size);
 
-    var pizzaOne = new Pizza(pizzaSize);
+    var toppings = [];
+    $("input:checkbox[name=toppings]:checked").each(function() {
+      toppings.push(this.value);
+      console.log(toppings);
+    });
+
+
+    //create new pizza
+    var pizzaOne = new Pizza(size, toppings);
+    console.log(pizzaOne);
+    //calculate the cost
     pizzaOne.sizeCost();
+      console.log(this.PizzaCOst);
+    pizzaOne.toppingsCost();
+    console.log(this.pizzaCost);
+
+    //display the cost
 
   });
-
-  $("toppings-form#pizza-toppings").submit(function(event) {
-    event.preventDefault();
-    var pizzaToppings = $("input#pizza-toppings").val();
-  });
-  pizza1.toppingsCost();
-
 });
