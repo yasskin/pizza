3628fcc25aeeeffecd7f64ca02cea15c179b97e6
Noah Yasskin <yasskin@icloud.com>
2016-06-17 13:55:21 -0700

    add a form for delivery in html and JS UI

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,9 @@
 // Business (or back-end) logic:
-function Pizza (size, toppings) {
+function Pizza (size, toppings, delivery) {
   this.pizzaSize = size;
   this.pizzaToppings = toppings;
   this.pizzaCost = 0;
+  this.pizzaDelivery = delivery;
 }
 
 Pizza.prototype.sizeCost = function() {
@@ -33,31 +34,39 @@ Pizza.prototype.toppingsCost = function () {
   }
 }
 
-// Everything below this line is user interface (or front-end) logic:
+Pizza.prototype.toppingsNumber = function () {
+  var toppingsNumber = this.pizzaToppings.length;
+}
 
+// user interface (or front-end) logic:
 $(document).ready(function() {
-  $("form").submit(function(event) {
+  $("form#pizza-order").submit(function(event) {
     event.preventDefault();
     //collect the user choices
-    var size = $("input:radio[name='size']:checked").val();
-
+    var size = $("input:radio[name=size]:checked").val();
     var toppings = [];
-    $("input:checkbox[name='toppings']:checked").each(function() {
+    $("input:checkbox[name=toppings]:checked").each(function() {
       (toppings).push(this.value);
     });
 
-
     //create new pizza
     var pizzaOne = new Pizza (size, toppings);
+    var toppingsNumber;
     //calculate the cost
     pizzaOne.sizeCost();
     pizzaOne.toppingsCost();
-    console.log(pizzaOne);
+    pizzaOne.toppingsNumber();
     //display the order with cost
     $("#show-order").show();
     $("#show-size").text(pizzaOne.pizzaSize);
-    $("#show-toppings").text(pizzaOne.pizzaToppings);
     $("#show-cost").text(pizzaOne.pizzaCost);
+    $("#show#delivery-details").show();
+  });
+  //add a delivery option
+  $("form#request-delivery").submit(function(event) {
+    event.preventDefault();
+
+    $("#delivery-details").show();
 
   });
 });
